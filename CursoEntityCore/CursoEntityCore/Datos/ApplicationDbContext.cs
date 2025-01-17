09e0f597dc16fbd1960c521cdab9c9ff58416850
RobertoVallejo <vallejoroberto19@gmail.com>
2025-01-16 23:36:36 -0500

    Seccion 1 terminado.

---
+++ CursoEntityCore/CursoEntityCore/Datos/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoEntityCore.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opciones) : base(opciones)
+        {
+                
+        }
+
+        //Escribir los modelos
+    }
+}
