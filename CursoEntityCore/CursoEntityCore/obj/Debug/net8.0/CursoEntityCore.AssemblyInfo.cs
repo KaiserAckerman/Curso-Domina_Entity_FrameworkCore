c552eb0c10055572a209e8a47e13cb50b8ddce7d
RobertoVallejo <vallejoroberto19@gmail.com>
2025-01-28 00:25:39 -0500

    Se personalizo el Home

--- CursoEntityCore/CursoEntityCore/obj/Debug/net8.0/CursoEntityCore.AssemblyInfo.cs
+++ CursoEntityCore/CursoEntityCore/obj/Debug/net8.0/CursoEntityCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoEntityCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ce4549b5d133d499cc47f9ac163a06966c22efc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab690a14ebc415224b84f9aed92036111af6d09b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoEntityCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoEntityCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
